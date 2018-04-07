d1dd3bf10d9883e453e74a9953d5f6d2a9f0c49f
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2018-04-07 13:53:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
+
+app = Flask(__name__, static_url_path="/static")
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 8mb
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
